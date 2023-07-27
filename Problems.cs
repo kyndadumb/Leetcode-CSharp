3b3ac7aee0b06ffda6ec08f1aeb296441b3dad42
kyndadumb <46259262+kyndadumb@users.noreply.github.com>
2023-07-27 07:26:12 +0200

    Problem 01 - TwoSum

---
+++ Problems.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeProblems;
+
+public class Problems
+{
+    // Problem: 01 - TwoSum
+    internal static int[] twoSum(int[] nums, int tagret)
+    {
+        // Dictionary für Zahlen mit Indizes
+        Dictionary<int,int> map = new();
+
+        // for - jedes Element von nums durchlaufen
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // num = Wert von aktuellen nums[i] zum Target
+            int num = tagret - nums[i];
+
+            // if - num bereits in Dictionary
+            if (map.TryGetValue(num, out int index))
+            {
+                // Array aus Indizes der Zahlen zurückgeben
+                return new int[] {i, index};
+            }
+
+            // aktuelle num in das Dictionary hinzufügen
+            else 
+            {
+                map[nums[i]] = i;
+            }
+        }
+
+        // falls keine Übereinstimmung - leeres Array zurückgeben
+        return new int[2];
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TwoSumTest();
+    }
+
+    public static void TwoSumTest()
+    {
+        int target = 19;
+        
+        Console.WriteLine("01 - Two Sum");
+
+        int[] nums = {1,2,3,4,5,6,7,8,9,10,35};
+        int[] result = Problems.twoSum(nums, target);
+
+        Console.WriteLine($"Result: {nums[result[0]]} + {nums[result[1]]} = {target}");
+    }
+}
